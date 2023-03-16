52505f6bbd6ab31381ccbdc6b802fb38ab88749d
Henry <henrysungyun@gmail.com>
2023-03-15 20:53:51 -0700

    finish

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,12 @@
 # your code goes here!
+
+class Anagram:
+    def __init__(self, word):
+        self.word = word.lower()
+        self.sorted_word = sorted(self.word)
+    def match(self, possible_anagrams):
+        matches = []
+        for possible_anagram in possible_anagrams:
+            if possible_anagram.lower() != self.word and sorted(possible_anagram.lower()) == self.sorted_word:
+                matches.append(possible_anagram)
+        return matches
